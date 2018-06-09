1a0d95e4344d01d021fc6a1fe783b9ac239c7ee5
Dorumolnar <35419205+Dorumolnar@users.noreply.github.com>
2018-06-09 15:26:38 +0200

    Update config.js

--- config.js
+++ config.js
@@ -4,9 +4,9 @@
 
 module.exports = {
   // Bot user token, found on Discord Devs website:
-  token: '',
+  token: 'NDU0OTk1NzM3MTUxNzk5MzM3.Df1jYw.0OtvOZJVEsY4fj0OalSzBX23wQw',
   // The bot app's client ID:
-  clientId: '',
+  clientId: '454995737151799337',
   // Commands should be preceded by this string:
   prefix: '@mention',
   // Moment.js date format to use:
